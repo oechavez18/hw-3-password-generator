c980b188309106944190a7ab15fd2751d101acea
Oscar Chavez <chavezeoscar@gmail.com>
2021-08-19 11:51:26 -0700

    Added added develp and assets folders

---
+++ Develop/script.js
@@ -0,0 +1,34 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+// Function for random generator
+function getRandomLower(){
+  return String.fromCharCode(Math.floor(Math.random() * 26)+97);
+}
+
+function getRandomUpper(){
+  return String.fromCharCode(Math.floor(Math.random() * 26)+65);
+}
+
+function getRandomNumber(){
+  return String.fromCharCode(Math.floor(Math.random() * 10)+48);
+}
+
+function getRandomCharacter(){
+  return String.fromCharCode(Math.floor(Math.random()*15 )+33);
+}
+
+console.log(Math.floor(Math.random()))
