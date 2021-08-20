fdd43b151dcbb1b05034d40c765a421798aff76c
Oscar Chavez <chavezeoscar@gmail.com>
2021-08-19 19:22:06 -0700

    add prompt screen for UI

--- Develop/script.js
+++ Develop/script.js
@@ -13,11 +13,6 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generatePassword(){
-  if (){
-    ;
-  }
-}
 
 // Function for random generator
 function getRandomLower(){
@@ -36,4 +31,51 @@ function getRandomCharacter(){
   return String.fromCharCode(Math.floor(Math.random()*15 )+33);
 }
 
-console.log(Math.floor(Math.random()))
+var randomGenerator= function(){
+   String.fromCharCode(Math.floor(Math.random() * 26)+97);
+   String.fromCharCode(Math.floor(Math.random() * 26)+65);
+   String.fromCharCode(Math.floor(Math.random() * 10)+48);
+   String.fromCharCode(Math.floor(Math.random()*15 )+33);
+};
+
+var passwordLength; 
+var confirmLower;
+var confirmUpper;
+var confirmNumber;
+var confirmCharacter;
+var selectedCriteria;
+
+
+function generatePassword(){
+   passwordLength= prompt("Please choose the length of your password, between 8-128 characters long.");
+   console.log("password length " + passwordLength);
+
+  if (!passwordLength)
+  {
+    alert("Invalid! Need to insert a number between 8-128.")
+  }
+  else if (passwordLength<8 || passwordLength>128)
+   {
+     passwordLength= alert("Password needs to be between 8-128 characters");
+     console.log("passwrod length " + passwordLength);
+   }
+
+   else{
+    confirmLower= confirm("Will your password take lower-case letters?");
+    console.log("Lower "+ confirmLower);
+    confirmUpper= confirm("Will your password take upper-case letters?");
+    console.log("Upper "+ confirmUpper);
+    confirmNumber= confirm("Will your password take numbers?");
+    console.log("Number "+ confirmNumber);
+    confirmCharacter= confirm("Will your password take special characters?");
+    console.log("Character "+ confirmCharacter);
+   }
+
+   if(!confirmLower && !confirmUpper && !confirmNumber && !confirmCharacter)
+   { alert("A criteria needs to be picked!  Please pick a criteria." );}
+   else if (confirmLower && confirmUpper && confirmNumber && confirmCharacter)
+{
+  selectedCriteria= randomGenerator() ;
+  
+}
+  }
